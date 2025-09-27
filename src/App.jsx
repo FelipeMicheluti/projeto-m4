ca7c85e2ecf365aee0f272d476004f5eb583feca
Felipe Micheluti <michelutifelipe@gmail.com>
2025-09-26 21:16:41 -0300

    26/09

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Card } from "./components/Card";
+import { Hello } from "./components/Hello";
+
+
+const CARDS = [
+  { id: 0, title: 'Card A', description: 'lala' },
+  { id: 1, title: 'Card B', description: 'kkk' },
+  { id: 2, title: 'Card C', description: 'hoh' },
+]
 const App = () => {
   return (
     <main className="min-h-dvh grid place-items-center bg-slate-50">
@@ -6,13 +15,17 @@ const App = () => {
       </h1>
 
       <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <div className="rounded-2xl border p-4 bg-slate-400 text-neutral-50 shadow-sm">card A </div>
-        <div className="rounded-2xl border p-4 bg-slate-400 text-neutral-50 shadow-sm">card B</div>
-        <div className="rounded-2xl border p-4 bg-slate-400 text-neutral-50 shadow-sm">card C</div>
+        {CARDS.map(item => (
+          <Card key={item.id} title={item.title}>
+            <p>{item.description}</p>
+          </Card>
+        ))}
       </div>
 
       <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-500 focus:ring-2 focust:ring-blue-500">link com foco</a>
-      
+
+      <Hello name="Micheluti" />
+
     </main>
 
   );
---
+++ src/components/Hello/index.jsx
@@ -0,0 +1,3 @@
+export const Hello = ({ name = "Mentes Brilhantes" }) => {
+    return <h2 className="text-xl">Olá, {name} 🤞(┬┬﹏┬┬)🤞</h2>
+}
